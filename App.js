fa11782c7e08b73c671be60081b4834b8153efe3
Alvee <doubleabatteryalvarez@gmail.com>
2018-09-21 01:33:27 -0600

    v0.0.3 Added form components -text input -single selection -multi selection *Selection components have optional inputs
    Added navigation between forms
    -Added Personal form
    -Added Test Form

--- App.js
+++ App.js
@@ -1,179 +1,319 @@
-import React, { Component } from 'react';
-import {
-  Button,
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableHighlight,
-  View,
-} from 'react-native';
+import React from 'react';
+import { Button, ScrollView, StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
 import { StackNavigator, createStackNavigator } from 'react-navigation'
 
-const PersonalForm = [
-  {
-    type: 'singleTextInput',
-    title: 'First Name',
-  },
-  {
-    type: 'singleTextInput',
-    title: 'Last Name',
-  },
-  {
-    type: 'singleTextInput',
-    title: 'Middle Initial',
-    placeHolder: '(Optional)',
-    required: false,
-  }
-];
+const TestForm = () => {
+    return (
+      <View style={styles.formContainer}>
+        <CompTextInput
+          name='Single Text Input'
+          placeHolder='Input PlaceHolder'/>
+        <CompMultiSelection
+            name='Multiple Selection'
+            options={['First Option', 'Second Option', 'Third Option', 'Option Accepts Input']}
+            valuesAcceptInput={['Option Accepts Input']}/>
+        <CompSingleSelection
+          name='Single Selection'
+          options={['First Option', 'Second Option', 'Third Option', 'Option Accepts Multiline']}
+          valuesAcceptInput={['Option Accepts Multiline']}
+          hasMultiline={true}/>
+      </View>
+    );
+}
 
-const TestForm = [
-  {
-    type: 'multiSelector',
-    title: 'This is a test',
-    choices: ['oops', 'thanks', 'bye'],
-  }
-]
-
-const ContactForm = [
-  {
-    type: 'singleTextInput',
-    title: 'Email',
-  },
-  {
-    type: 'singleTextInput',
-    title: 'Main Phone #',
-  },
-  {
-    type: 'singleTextInput',
-    title: 'Alt. Phone #',
-    placeHolder: '(Optional)',
-    required: false,
-  }
-];
-
-const PersonalPage = {
-  title: 'Personal Info',
-  formList: [PersonalForm, ContactForm, TestForm]
-};
-
-function singleTextInput(singleText) {
-  let value = '';
-  return (
-      <TextInput
-        style={styles.textInput}
-        placeHolder={('placeholder' in singleText) ? singleText.placeHolder : ''}
-        onChangeText={(text) => {this.value=text}}
-        value={this.value}
-      />
+const PersonalForm = () => {
+  return(
+    <View>
+      <View style={styles.formContainer}>
+        <CompTextInput name='First Name'/>
+        <CompTextInput name='Last Name'/>
+        <CompTextInput name='Middle Initial' placeHolder='(Optional)'/>
+        <CompSingleSelection
+          name='Have you ever used another name(s)?'
+          options={['No', 'Yes']}
+          default='No'
+          valuesAcceptInput={['Yes']}
+          hasMultiline={true} />
+      </View>
+      <View style={styles.formContainer}>
+        <CompTextInput name='Main Telephone'/>
+        <CompTextInput name='Alt. Telephone' placeHolder='(Optional)'/>
+        <CompTextInput name= 'Email Address' />
+      </View>
+      <View style={styles.formContainer}>
+        <CompTextInput name='SSN'/>
+        <CompTextInput name='Drivers License #'/>
+        <CompSingleSelection
+          name='Have you ever been exposed to bed bugs or a similar bug infestation?'
+          options={['No', 'Yes, explain:']}
+          default='No'
+          valuesAcceptInput={['Yes, explain:']}
+          hasMultiline={true} />
+      </View>
+    </View>
   );
 }
 
-function multiSelect(multiSelector) {
-  let value = '';
-  select = (newVal) =>
-  {
-    this.value = newVal;
-    //alert(newVal);
-  };
-
-  return (
-    multiSelector.choices.map((choice) => {
-      return(
-
-        <TouchableHighlight onPress={this.select(choice)}>
-          <View style={styles.multiChoice}>
-            <Image
-              style={styles.multiChoiceIcon}
-              source={require('./assets/multiChoiceIcon.png')}
-            />
-            <Text style={styles.multiChoiceLabel}>{choice}</Text>
-          </View>
-        </TouchableHighlight>
-
-      );
-    })
-
-  );
+export default class App extends React.Component {
+ render() {
+   return <RootStack />;
+ }
 }
 
-class FormPage extends React.Component {
-  constructor() {
-    super();
-    this.page = PersonalPage;
-  }
 
-  renderComponent (formComp) {
-    switch (formComp.type) {
-      case 'singleTextInput':
-        return (
-          <View style={styles.formCompContainer}>
-          <Text
-          fontSize={('fontSize' in formComp) ? formComp.fontSize : styles.formSectionTitle}
-          style={styles.formSectionTitle}>
-            {('title' in formComp) ? formComp.title : ''}
-          </Text>
-            {singleTextInput(formComp)}
-          </View>
-        );
-        break;
-      case 'multiSelector':
-        return (
-          <View style={styles.formCompContainer}>
-          <Text
-          fontSize={('fontSize' in formComp) ? formComp.fontSize : styles.formSectionTitle}
-          style={styles.formSectionTitle}>
-            {('title' in formComp) ? formComp.title : ''}
-          </Text>
-            {multiSelect(formComp)}
-          </View>
-        );
-        break;
+class FormNavigator extends React.Component {
+
+  constructor (props)
+  {
+    super(props);
+    this.state = {
+      formGroups: [{
+        groupName: 'Personal Info',
+        forms: [{
+          name: 'Personal Info',
+          form: PersonalForm()
+        },{
+          name: 'Test Form',
+          form: TestForm()
+        }]
+      },{
+        groupName: 'Test Form Group',
+        forms: [{
+          name:'Test Form',
+          form: TestForm()
+        }]
+      }],
     }
   }
 
-  renderForm (form) {
-    return (
-      <View style={styles.formContainer}>
-        {form.map((formComp) => this.renderComponent(formComp))}
+  render() {
+    return (this.state.formGroups.map((group) =>
+      <View style={styles.formContainer} key={group.groupName}>
+        {group.forms.map((form) =>
+          <Button
+            title={form.name}
+            style={styles.navButton}
+            onPress={() => this.props.navigation.navigate('Form', {
+              name: form.name,
+              renderForm: form.form,
+            })}
+            key={form.name}
+          />)}
       </View>
-    );
+    ));
   }
+}
+
+class Form extends React.Component {
 
   render() {
+    const { navigation } = this.props;
+    const renderForm = navigation.getParam('renderForm', TestForm())
+    const name = navigation.getParam('name', 'REQUIRES NAME')
+
     return (
       <ScrollView>
-        <View style={styles.headContainer}>
-          <Text style={styles.headText}>{this.page.title}</Text>
-        </View>
-          {this.page.formList.map((form) => this.renderForm(form))}
-        <View style={styles.footContainer}>
+        <View>
+          <View style={styles.headContainer}>
+            <Text style={styles.headText}>{name}</Text>
+          </View>
+          {renderForm}
           <Button
-            title="Submit"
-            onPress={() => this.props.navigation.navigate('Form_Main')}
-          />
-        </View>
+            style={styles.navButton}
+            title='Save'
+            onPress={() => this.props.navigation.goBack()} />
+          </View>
       </ScrollView>
+
     );
   }
 }
 
-class FormMain extends React.Component {
+class CompTextInput extends React.Component{
+  constructor (props) {
+    super(props);
+    this.state = {value: ''};
+  }
 
   render() {
-    return (
-      <View>
-        <Button
-          title="Personal Info"
-          onPress={() => this.props.navigation.navigate('Form_Page')}
+    return(
+      <View style={styles.formCompContainer}>
+        <Text style={styles.formCompTitle}>{this.props.name}</Text>
+        <TextInput
+          style={styles.textInput}
+          onChange={(newVal) => {this.props.state = newVal}}
+          placeHolder={this.props.placeHolder}
+          multiline={this.props.multiline}
         />
       </View>
     );
   }
 }
 
+class CompSingleSelection extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.default,
+      inputValue: '',
+    };
+    this.selectedValue = '';
+  }
+
+  _onChange = (newVal) => {
+    newVal = (this.state.value === newVal) ? '' : newVal;
+    this.selectedValue = newVal;
+    this.setState({value: newVal});
+  }
+
+  render() {
+
+    return (
+      <View style={styles.formCompContainer}>
+        <Text style={styles.formCompTitle}>{this.props.name}</Text>
+        {this.props.options.map((option) =>
+          <CompSingleOption
+            name={option}
+            key={option}
+            onChange={this._onChange}
+            selectedValue={this.selectedValue}
+            input={(this.props.valuesAcceptInput.indexOf(option) > -1) ? (
+              <TextInput
+                style={(this.props.hasMultiline) ? styles.multilineTextInput : styles.textInput}
+                onChangeText={(inputValue) => this.setState({inputValue})}
+                value={this.state.inputValue}
+                multiline={this.props.hasMultiline}
+              />) : null}
+          />)}
+      </View>
+    );
+  }
+}
+
+class CompMultiSelection extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: new Array(),
+      inputValue: ''
+    };
+  }
+
+  _onChange = (newVal) => {
+
+    let valIndex = this.state.values.indexOf(newVal);
+    let prevValues = new Array();
+
+    if (valIndex > -1){
+      prevValues.splice(valIndex,1);
+    }
+    else {
+      prevValues.push(newVal);
+    }
+
+    this.setState({values: prevValues});
+  }
+
+  render() {
+    return (
+      <View style={styles.formCompContainer}>
+        <Text style={styles.formCompTitle}>{this.props.name}</Text>
+        {this.props.options.map((option) =>
+          <CompMultiOption
+            name={option}
+            onChange={this._onChange}
+            key={option}
+            input={(this.props.valuesAcceptInput.indexOf(option) > -1) ? (
+              <TextInput
+                style={(this.props.hasMultiline) ? styles.multilineTextInput : styles.textInput}
+                onChangeText={(inputValue) => this.setState({inputValue})}
+                value={this.state.inputValue}
+                multiline={this.props.hasMultiline}
+              />) : null}
+          />)}
+      </View>
+    );
+  }
+}
+
+class CompSingleOption extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+
+    this.showInput = false;
+  }
+
+  _onPress = () => {
+      this.props.onChange(this.props.name);
+  }
+
+  render() {
+    return (
+      <TouchableHighlight onPress={this._onPress} underlayColor='#e5e5e5' >
+        <View>
+          <View style={styles.multiOption}>
+            <View style={styles.multiOptionIcon} backgroundColor={(this.props.selectedValue === this.props.name) ? '#5D737F' : styles.multiOptionIcon.backgroundColor}/>
+            <Text style={styles.multiOptionLabel}>{this.props.name}</Text>
+          </View>
+          {(this.props.selectedValue === this.props.name) ? this.props.input : null}
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+class CompMultiOption extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false,
+      inputValue: '',
+    };
+    this.showInput = false;
+  }
+
+  _onPress = () => {
+      this.setState({selected: !this.state.selected});
+      this.props.onChange(this.props.name);
+  }
+
+  render() {
+    let input =
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(inputValue) => this.setState({inputValue})}
+        value={this.state.inputValue}
+      />;
+    return (
+      <TouchableHighlight onPress={this._onPress} underlayColor='#e5e5e5' >
+        <View>
+          <View style={styles.multiOption}>
+            <View style={styles.multiOptionIcon} backgroundColor={(this.state.selected) ? '#5D737F' : styles.multiOptionIcon.backgroundColor}/>
+            <Text style={styles.multiOptionLabel}>{this.props.name}</Text>
+          </View>
+          {(this.props.input != null && this.state.selected) ? this.props.input : null}
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    FormNavigator: FormNavigator,
+    Form: Form,
+  },
+  {
+    initialRouteName: 'FormNavigator',
+  }
+);
 
 const styles = StyleSheet.create({
   mainContainer: {
@@ -181,7 +321,7 @@ const styles = StyleSheet.create({
   },
 
   headContainer: {
-    marginTop: 30,
+    marginTop: 20,
     padding: 20,
     alignItems: 'center',
   },
@@ -205,10 +345,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent'
   },
 
-  formSectionTitle: {
+  formCompTitle: {
     padding: 5,
     marginLeft: 15,
-    marginBottom: 20,
+    marginBottom: 15,
     justifyContent: 'center',
 
     color: '#5D737F',
@@ -226,47 +366,46 @@ const styles = StyleSheet.create({
     fontFamily: 'serif',
   },
 
-  multiChoice: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
+  multilineTextInput: {
+    padding: 5,
+    marginLeft: 15,
+    marginBottom: 20,
+    justifyContent: 'center',
 
-  multiChoiceIcon: {
-    height: 30,
-    width: 30,
-    margin: 10,
-    color: 'blue'
-  },
-
-  multiChoiceLabel: {
     color: '#5D737F',
-    fontSize: 21,
-    fontFamily: 'serif'
+    fontSize: 18,
+    fontWeight: '100',
   },
 
   headText: {
     fontSize: 32,
     color: '#6E797F',
+  },
+
+  multiOption: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingLeft: 10,
+  },
+
+  multiOptionIcon: {
+    height: 25,
+    width: 25,
+    margin: 10,
+    backgroundColor: '#e5e5e5',
+    borderRadius: 15,
+  },
+
+  multiOptionLabel: {
+    color: '#5D737F',
+    fontSize: 18,
+    fontFamily: 'serif',
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+
+  navButton: {
+
   }
 });
-
-const RootStack = createStackNavigator(
-  {
-    Form_Main: {
-      screen: FormMain,
-    },
-    Form_Page: {
-      screen: FormPage,
-    },
-  },
-  {
-    initialRouteName: 'Form_Main',
-  }
-);
-
-export default class App extends React.Component {
- render() {
-   return <RootStack />;
- }
-}
